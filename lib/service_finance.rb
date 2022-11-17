e87d8e69adc8162511d51b625dbfb35ceb6bd52a
leandro.silva <leandro.silva@roit.com.br>
2022-11-16 22:07:17 -0300

    Yeaaah

---
+++ lib/service_finance.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "service_finance/version"
+
+module ServiceFinance
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/service_finance/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ServiceFinance
+  VERSION = "0.1.0"
+end
